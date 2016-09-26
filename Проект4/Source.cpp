fe25116b2d93188b4e9f1889945021062f2ca593
iozzyt <iozzyyt@gmail.com>
2016-09-26 13:38:02 +0300

    hf,jnftn!

--- Проект4/Source.cpp
+++ Проект4/Source.cpp
@@ -7,16 +7,24 @@ using namespace std;
 class Number		//класс number для хранения десятичных чисел 
 {
 private:
-	float fracpart, numb, res;
+	float fracpart, numb;
 	int intpart;
 public:
-
+	float res;
 	Number()		//конструктор по умолчанию
 	{
-			numb = 0;
-			fracpart = 0;
-			intpart = 0;
-			res = 0;
+		numb = 0;
+		fracpart = 0;
+		intpart = 0;
+		res = 0;
+	}
+	bool operator<(Number num)
+	{
+		return numb < num.numb;
+	}
+	friend std::ostream& operator<<(std::ostream& os, Number num)
+	{
+		return os << num.numb;
 	}
 
 	Number(float num)		//конструктор инициализации
@@ -51,53 +59,147 @@ public:
 	}
 	void getlastres()		//метод вывода последнего результата
 	{
-		cout << "last res: " << res << endl;
+		return res;
 	}
 	void getfracpart()		//output frac part
 	{
-		cout << "fracpart: " << fracpart << endl;
+		return fracpart;
 	}
 	void getintpart()		//output int part
 	{
-		cout << "intpart: " << intpart << endl;
+		return intpart;
 	}
-	void powerNumber(int power)
+	void powerNumber(int power)		//возведение объекта в степень
 	{
-		res = pow(numb,power);
-		cout << "powerNumber: " << res << endl;
+		res = pow(numb, power);
+		return res;
 	}
-	void xNumber(float x)
+	void xNumber(float x)		//умножение объекта на число
 	{
 		res = x*numb;
-		cout << "xNumber: " << res << endl;
+		return res;
 	}
 
-	void plusNumber(float x)
+	void plusNumber(float x)		//прибавление числа к объекту
 	{
 		res = numb + x;
-		cout << "plusNumber: " << res << endl;
+		return res;
 	}
-	
+
 	~Number()		//деструктор
 	{
 
 	}
 };
 
-class Tree
+template<class Item>
+class tree
 {
+private:
+	struct node
+	{
+		Item item;
+		node * left, *right;
+		node(Item item = Item()) :item(item)
+		{
+			left = right = nullptr;
+		}
+
+		static void add(node*& curr, Item &item)
+		{
+			if (!curr)
+			{
+				curr = new node(item);
+
+			}
+			else if (curr->item<item)
+				add(curr->left, item);
+			else
+				add(curr->right, item);
+		}
+		static node* deepcopy(node* from)
+		{
+			if (!from)return nullptr;
+			node * res = new node(from->item);
+			res->left = deepcopy(from->left);
+			res->right = deepcopy(from->right);
+			return res;
+		}
+		static void print(node*curr, std::ostream& os, std::string spacer = "")
+		{
+			if (curr)
+			{
+				print(curr->left, os, spacer + "   ");
+				os << &spacer[0];
+				os << curr->item << "\n";
+				print(curr->right, os, spacer + "   ");
+			}
+		}
+
+		static void clear(node*&curr)
+		{
+			if (curr)
+			{
+				clear(curr->left);
+				clear(curr->right);
+				delete curr;
+			}
+		}
+	}*head;
+public:
+	tree()
+	{
+		head = nullptr;
+	}
+	tree(const tree& t)
+	{
+		head = node::deepcopy(t.head);
+	}
+	tree operator=(tree t)
+	{
+		head = node::deepcopy(t.head);
+		return t;
+	}
+	friend std::ostream& operator<<(std::ostream& os, tree t)
+	{
+		node::print(t.head, os);
+		return os;
+	}
+
+	void add(Item item = Item())
+	{
+		node::add(head, item);
+	}
+	void clear()
+	{
+		node::clear(head);
+		head = nullptr;
+	}
+	~tree()
+	{
+		clear();
+	}
+
 
 };
 
 int main()
 {	
-	Number obj, obj1;
+	Number obj1;
 	obj1 = 12.7;
-	obj.getNumber();
+	obj1.getNumber();
 	obj1.getNumber();
 	obj1.getintpart();
 	obj1.getfracpart();
 	obj1.powerNumber(2);
+	tree<Number> obj;
+	obj.add(10);
+	obj.add(9);
+	obj.add(15);
+	obj.add(10);
+	obj.add(9);
+	obj.add(15);
+	cout << obj;
 	cout << "press enter for close" << endl;
 	getchar();
 	//system("pause");
